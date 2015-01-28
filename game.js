2538332fca28f302da26aa445fc8eeadfebbbf20
Irene <irene@aurametrix.com>
2015-01-27 17:14:35 -0800

    var to clear canvas

--- game.js
+++ game.js
@@ -14,3 +14,21 @@ var width = 320,
 c.width = width;
 c.height = height;
 //setting canvas size 
+
+var clear = function(){
+  ctx.fillStyle = '#d0e7f9';
+//set active color to #d0e... (nice blue)
+//UPDATE - as 'Ped7g' noticed - using clearRect() in here is useless, we cover whole surface of the canvas with blue rectangle two lines below. I just forget to remove that line
+//ctx.clearRect(0, 0, width, height);
+//clear whole surface
+  ctx.beginPath();
+//start drawing
+  ctx.rect(0, 0, width, height);
+//draw rectangle from point (0, 0) to
+//(width, height) covering whole canvas
+  ctx.closePath();
+//end drawing
+  ctx.fill();
+//fill rectangle with active
+//color selected before
+}

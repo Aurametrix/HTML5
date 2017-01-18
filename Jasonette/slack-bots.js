3b8086340ad7aa9d766950d2e4200224fb625a45
Aurametrix <irene@aurametrix.com>
2017-01-18 08:52:10 -0800

    mobile app for remote-controlling a Slack bot
    http://jasonette.com/
    http://blog.jasonette.com/2016/01/17/build-a-slackbot-with-jasonette/
    https://news.ycombinator.com/item?id=13417442

---
+++ Jasonette/slack-bots.js
@@ -0,0 +1,11 @@
+{
+  "type": "$network.request",
+  "options": {
+    "url": "https://evening-inlet-32428.herokuapp.com/submit.json",
+    "method": "POST",
+    "data": {
+      "user_name": "{{name}}",
+      "user_id": "{{id}}"
+    }
+  }
+}
